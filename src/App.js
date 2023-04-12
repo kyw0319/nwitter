e1318685c9c1f01897f82b07ad77b734ac456a79
kyw0319 <125557918+kyw0319@users.noreply.github.com>
2023-04-12 15:44:17 +0900

    1.0 initialization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
